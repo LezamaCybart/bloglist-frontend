e31aaabd562efc66331c0f761fec5cb45c8a0563
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-12-04 19:49:04 -0300

    step1 users can login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  console.log(user)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,6 +16,45 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(exception) {
+      console.log(exception)
+    }
+  }
+  if (user === null) {
+    return (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="text"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+      <button type="submit">login</button>
+      </form>
+
+    )
+  }
   return (
     <div>
       <h2>blogs</h2>
