5ee086f7d034a4dcda9ea5e412c34034aa193763
LezamaCybart <lezamacybartmatiasgerman@gmail.com>
2021-12-05 16:42:54 -0300

    step7 togglabe blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    Title: {blog.title} Author: {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+
+  const [infoVisible, setInfoVisible] = useState(false)
+
+  const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+  }
+  const hideWhenVisible = { display: infoVisible ? 'none' : '' }
+  const showWhenVisible = { display: infoVisible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <p>
+          {blog.title} 
+          <button onClick={() => setInfoVisible(true)}>view</button>
+        </p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>
+        {blog.title} 
+        <button onClick={() => setInfoVisible(false)}>hide</button>
+        </p>
+        <p>
+        {blog.url} 
+        </p>
+        <p>
+        likes: {blog.likes} 
+        </p>
+        <p>
+        {blog.author}
+        </p>
+      </div>
+    </div>  
+  )
+
+}
 
 export default Blog
